13a440563ad25111b887523b5a3fcc6793fa24bf
Arthur Bridges <killor@Arthurs-MBP.lan>
2023-09-11 22:41:34 -0500

    FINALIZED BEFORE UPDATE ON README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Password-Generator
-Webpage that is a Password Generator
+#Password Generator
+
+##Description
+
+A webpage that generates passwords based on user inputs. 
+
+##Installation
+
+N/A
+
+##Usage
+
+N/A
+
+##License 
+
+Credit to the author of this repo.
--- script.js
+++ script.js
@@ -3,65 +3,65 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_+=<>?";
     var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lowerCase = "abcdefghijklmnopqrstuvwxyz";
     var numericCharacters = "1234567890";
     var specialCharacters = "!@#$%^&*()-_+=<>?";
     var numOfCharacters;
     var characterTypes;
-    var selectedChoices;
+    var index;
+    var selectedChoices = "";
     var password = "";
     //TODO (9/8/2023)
     //Plan is to add all selected choices into container selectedChoices then randomize it after user choices.
-    
-    var numOfCharacters = prompt("Enter the length of characters(Between 8 and 128 characters): \n");
-    do  {
+   do{
+    numOfCharacters = prompt("Enter the length of characters(Between 8 and 128 characters): \n");
+  
     if(numOfCharacters >= 8 && numOfCharacters <= 128) {
-    
-      var characterTypes = confirm("Would you like to include character types(press 'ok' for yes)?: \n");
+    characterTypes = confirm("Would you like to include character types(press 'ok' for yes)?: \n");
+      
       if(characterTypes){
-        var upperCase = confirm("Would you like to inlude upper case characters within the password? \n");
-        var lowerCase = confirm("Would you like to inlude lower case characters within the password? \n");
-        var numericCharacters = confirm("Would you like to inlude numeric characters within the password? \n");
-        var specialCharacters = confirm("Would you like to inlude special characters within the password? \n");
         //concantenate strings if true into 'selectedChoices.
         
-        if(upperCase){
+        if(confirm("Would you like to include uppercase characters?")){
           //within the container 'password' concantenate the lowercase to the password via random.
           selectedChoices += upperCase;
         }
-        if(lowerCase){
+        if(confirm("Would you like to include uppercase characters?")){
            //within the container 'password' concantenate the uppercase to the password via random.
            selectedChoices += lowerCase;
         }
-        if(numericCharacters){
+        if(confirm("Would you like to include numeric characters?")){
           selectedChoices += numericCharacters;
         }
-        if(specialCharacters){
+        if(confirm("Would you like to include special characters?")){
            //within the container 'password' concantenate the variable specialCharacters to the password via random.
            selectedChoices += specialCharacters;
         }
         break;
     }
-    else if(!upperCase && !lowerCase && !numericCharacters && !specialCharacters) {
+    else if (!upperCase && !lowerCase && !numericCharacters && !specialCharacters) {
       console.log("You must select at least one choice \n");
       continue;
     }
   }
 else {
   console.log("The password length must be between 8 and 128, please try again. \n");
+  continue;
 }
-} while(true);
+}while(true);
+
 
   //randomize selected choices
 
   for (var i = 0; i < numOfCharacters; i++) {
     /*This line of code is where I will include the math.random and/or math.floor function
     to randomize selectedChoices then place the 
-    selectedChoices container into the container password then return password.*/ 
-    password += selectedChoices;
+    selectedChoices container into the container password then return password.*/
+    var index = Math.floor(Math.random() * selectedChoices.length);
+    password += selectedChoices.charAt(index);
   }
+  return password;
 }//function closing.
 
 
